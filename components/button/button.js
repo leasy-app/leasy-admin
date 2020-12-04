b63342be1847d261134c1b338da1bd5c843fd34e
Amin Rezaei <Amin.Rezaei1379@gmail.com>
2020-12-04 19:20:25 +0330

    Cats Add

--- components/button/button.js
+++ components/button/button.js
@@ -4,9 +4,9 @@ export default function Button({icon, value, trigger}){
     return <div className={styles.button} onClick={()=>{
        trigger && trigger();
     }}>
-        <div className={styles.icon}>
+        {icon ? <div className={styles.icon}>
             {icon}
-        </div>
+        </div> : undefined}
         <div className={styles.text}>
             {value}
         </div>
--- components/pages/cats.js
+++ components/pages/cats.js
@@ -1,7 +1,11 @@
 import styles from './cats.module.scss';
+import Input from "../input/input";
+import Button from "../button/button";
 import cn from "classnames";
+import {useState} from "react";
 
 export default function CatsPage({className}){
+    const [state, setState] = useState({});
     return <div className={cn({
         [styles.page]: true,
         [className]: !!className,
@@ -17,7 +21,16 @@ export default function CatsPage({className}){
             <CatTile image={"/images/test-back.jpg"} name={"Nature"}/> 
             <CatTile image={"/images/test-back.jpg"} name={"Nature"}/> 
         </div>
-        <div className={styles.addCat}>
+        <div className={styles.addCat}>     
+            <span className={styles.categoryTitle}>
+                    {"Add Category"}
+            </span>
+            <div className={styles.addCatImage}>
+                
+            <img className={styles.addCatIcon} src={"/images/add-image.png"}/>
+            </div>
+            <Input hint={"Name"} value={state.name}/>
+            <Button value={"Add"} />
         </div>
     </div>
 }
