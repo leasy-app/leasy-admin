c64de31040d92bacc96191f64cd299abff7cdeac
Amin Rezaei <Amin.Rezaei1379@gmail.com>
2020-12-22 18:36:53 +0330

    categories input implement + preview

--- components/pages/cats.js
+++ components/pages/cats.js
@@ -2,10 +2,15 @@ import styles from './cats.module.scss';
 import Input from "../input/input";
 import Button from "../button/button";
 import cn from "classnames";
-import {useState} from "react";
+import {useRef, useState} from "react";
 
 export default function CatsPage({className}){
-    const [state, setState] = useState({});
+    const [state, setState] = useState({
+        tgFile: "",
+        prev: null,
+    });
+    const fileInput = useRef(null);
+
     return <div className={cn({
         [styles.page]: true,
         [className]: !!className,
@@ -25,9 +30,22 @@ export default function CatsPage({className}){
             <span className={styles.categoryTitle}>
                     {"Add Category"}
             </span>
-            <div className={styles.addCatImage}>
+            <div className={styles.addCatImage} onClick={()=>{
+                fileInput.current.click();
+            }}>
                 
-            <img className={styles.addCatIcon} src={"/images/add-image.png"}/>
+            {state.tgFile ? 
+            <img className={styles.catImage} src={state.prev}/>
+            :
+            <img className={styles.addCatIcon} src={"/images/add-image.png"}/>}
+            <input type="file" className={styles.tradInput} ref={fileInput} onChange={e => {
+                let f = e.target.files[0];
+                let reader = new FileReader();
+                reader.onload = function (e) {
+                    setState({...state, tgFile:f, prev: e.target.result});
+                }
+                reader.readAsDataURL(f);
+            }}/>
             </div>
             <Input hint={"Name"} value={state.name}/>
             <Button value={"Add"} />
