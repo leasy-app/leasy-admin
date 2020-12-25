a6a0e5032d60190833ac3fb71d22831045917186
Amin Rezaei <Amin.Rezaei1379@gmail.com>
2020-12-26 01:40:39 +0330

    connect stats to api

--- components/pages/stats.js
+++ components/pages/stats.js
@@ -1,17 +1,29 @@
 import styles from './stats.module.scss';
 import cn from "classnames";
+import { useStats } from '../../lib/logic/api';
 
 export default function StatsPage({className}){
+    let {isError, isLoading, stats} = useStats();
+    let title;
+    if(isError) title = "Unexpected Error Occured:(";
+    else if(isLoading) title = "Loading...";
+    else title = "Statistics";
+    let incomplete = isError || isLoading;
     return <div className={cn({
         [styles.page]: true,
         [className]: !!className,
     })}>
-        <div className={styles.tileRow}>
-            <StatTile icon={"/images/verified.png"} value={14000} name={"Users"}/>
-            <StatTile icon={"/images/post.png"} value={200} name={"Posts"}/>
-            <StatTile icon={"/images/edit.png"} value={70} name={"Authors"}/>
-            <StatTile icon={"/images/feather.png"} value={30000} name={"Reads"}/>
-        </div>
+        <span className={styles.title}>
+            {title}
+        </span>
+        {incomplete ? undefined : 
+            <div className={styles.tileRow}>
+                <StatTile icon={"/images/verified.png"} value={stats.users} name={"Users"}/>
+                <StatTile icon={"/images/post.png"} value={stats.posts} name={"Posts"}/>
+                <StatTile icon={"/images/graduation.png"} value={stats.courses} name={"Courses"}/>
+                <StatTile icon={"/images/feather.png"} value={stats.reads} name={"Reads"}/>
+            </div>
+    }
     </div>
 }
 
