d931d00997e6513ca2870313e0385a8df148d93e
Amin Rezaei <Amin.Rezaei1379@gmail.com>
2020-12-25 04:02:13 +0330

    connect posts to api

--- components/post/post.js
+++ components/post/post.js
@@ -1,5 +1,8 @@
 import styles from "./post.module.scss";
 import cn from "classnames";
+import { imageAddr } from "../../lib/logic/api";
+
+const truncate = (input, len) => input.length > len ? `${input.substring(0, len)}...` : input;
 
 export default function Post({post, className, divider}){
     return <div className={cn({
@@ -8,14 +11,14 @@ export default function Post({post, className, divider}){
     })}>
         <div className={styles.namesHolder}>
             <span className={styles.title}>{post.title}</span>
-            <span className={styles.description}>{post.desc}</span>
+            <span className={styles.description}>{truncate(post.desc, 60)}</span>
             <div className={styles.authorHolder}>
-                <img className={styles.authorPhoto} src={post.authorPhoto}/>
+                <img className={styles.authorPhoto} src={imageAddr(post.authorPhoto)}/>
                 <span className={styles.authorName}>{post.author}</span>
                 <span className={styles.bullet}>•</span>
                 <span className={styles.date}>{post.date}</span>
             </div>
         </div>
-        <img className={styles.image} src={post.image}/>
+        <img className={styles.image} src={imageAddr(post.image)}/>
     </div>
 }
