bf83ead2351bb4ad8cdab309f4a67b6c825bfa8b
Amin Rezaei <Amin.Rezaei1379@gmail.com>
2020-12-26 01:19:57 +0330

    connect courses to api

--- components/course/course.js
+++ components/course/course.js
@@ -1,5 +1,6 @@
 import styles from "./course.module.scss";
 import cn from "classnames";
+import { imageAddr } from "../../lib/logic/api";
 
 export default function Course({course, className, divider}){
     return <div className={cn({
@@ -7,15 +8,15 @@ export default function Course({course, className, divider}){
         [className]: !!className,
     })}>
         <div className={styles.namesHolder}>
-            <span className={styles.title}>{course.title}</span>
-            <span className={styles.description}>{course.desc}</span>
-            <div className={styles.authorHolder}>
+            <span className={styles.title}>{course.Name}</span>
+            <span className={styles.description}>{course.Explanation}</span>
+            {/* <div className={styles.authorHolder}>
                 <img className={styles.authorPhoto} src={course.authorPhoto}/>
                 <span className={styles.authorName}>{course.author}</span>
                 <span className={styles.bullet}>•</span>
                 <span className={styles.date}>{course.date}</span>
-            </div>
+            </div> */}
         </div>
-        <img className={styles.image} src={course.image}/>
+        <img className={styles.image} src={imageAddr(course.Picture)}/>
     </div>
 }
--- components/pages/courses.js
+++ components/pages/courses.js
@@ -1,25 +1,33 @@
 import styles from "./courses.module.scss";
 import cn from "classnames";
 import Course from "../course/course";
+import { useCourses } from "../../lib/logic/api";
 
 export default function CoursesPage({className}){
-    const courses = [
-        {title:"Neural Network Optimizers",
-         desc:"Analyzing and Learning Neural Network Optimizers like Adam, AdaBelief, ...",
-         authorPhoto:"images/user-photo.png",
-          author:"Emma Hart", date:"Oct 15", image:"images/test-back.jpg"},
-    ];
-    [...Array(10).keys()].forEach(_ => courses.push(courses[0]));
+    // const courses = [
+    //     {title:"Neural Network Optimizers",
+    //      desc:"Analyzing and Learning Neural Network Optimizers like Adam, AdaBelief, ...",
+    //      authorPhoto:"images/user-photo.png",
+    //       author:"Emma Hart", date:"Oct 15", image:"images/test-back.jpg"},
+    // ];
+    // [...Array(10).keys()].forEach(_ => courses.push(courses[0]));
+    let {isError, isLoading, courses} = useCourses();
+    let title;
+    if(isError) title = "Unexpected Error Occured:(";
+    else if(isLoading) title = "Loading...";
+    else title = "Latest Courses";
+    let incomplete = isError || isLoading;
     return <div className={cn({
         [styles.page]: true,
         [className]: !!className,
     })}>
 
         <span className={styles.title}>
-            {"Latest Courses"}
+            {title}
         </span>
         <div className={styles.coursesHolder}>
         {
+            incomplete ? undefined :
             courses.map(course => <Course course={course}/>)
         }
         </div>
